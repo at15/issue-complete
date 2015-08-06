61e4b74b4c7816b91602e04af3753bdb7d4502cc
at15 <at15@outlook.com>
2015-08-07 00:04:16 +1000

    Can parse github origin repo now
    - get user name
    - get repo name
    - get issue api url

--- ftplugin/COMMIT_EDITMSG/issue.py
+++ ftplugin/COMMIT_EDITMSG/issue.py
@@ -1,10 +1,13 @@
 import json
 import requests
 import vim
+import re
 
 
 class GitRepo:
     config = ""
+    user_name = ""
+    repo_name = ""
 
     def __init__(self, config_file):
         f = open(config_file, 'r')
@@ -15,12 +18,29 @@ class GitRepo:
     def parse_config(self, content):
         # Read the config and parse
         # TODO: parse and get the url
-        print content
+        lines = content.split('\n')
+        remote_url = ""
+        # loop to find the line with remote origin
+        for i in range(len(lines)):
+            if lines[i] == '[remote "origin"]':
+                remote_url = lines[i + 1]
+                break
+        print "remote url is " + remote_url
+        pattern = re.compile(r'.*github.com[:/](.*)/(.*).git')
+        match = pattern.match(remote_url)
+        user_name = match.group(1)
+        repo_name = match.group(2)
+        self.user_name = user_name
+        self.repo_name = repo_name
+
+    def issue_url(self):
+        # TODO: error handling
+        return "https://api.github.com/repos/" + self.user_name + "/" + self.repo_name + "/issues"
 
 def hi():
     # url = "https://api.github.com/repos/at15/issue-complete/issues"
     # r = requests.get(url)
-    #print r.json()
+    # print r.json()
     #print "Hi !"
     repo()
 
--- playground/parse.py
+++ playground/parse.py
@@ -1,7 +1,31 @@
 __author__ = 'at15'
+
+import re
+
+
 def parse():
     f = open('config', 'r')
     content = f.read()
-    print content
+    lines = content.split('\n')
+    remote_url = ""
+    # loop to find the line with remote origin
+    for i in range(len(lines)):
+        if lines[i] == '[remote "origin"]':
+            remote_url = lines[i + 1]
+            break
+    print remote_url
+    # TODO: support http
+    # https://github.com/ParsePlatform/parse-cli.git
+    # git@github.com:ParsePlatform/parse-cli.git
+    # remote_url = 'git@github.com:ParsePlatform/parse-cli.git'
+    pattern = re.compile(r'.*github.com[:/](.*)/(.*).git')
+    match = pattern.match(remote_url)
+    # print match
+    user_name = match.group(1)
+    repo_name = match.group(2)
+    print user_name
+    print repo_name
+    # get user and repo from the url
+
 
 parse()
